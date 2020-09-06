ff9b37599f7d8dc86a599bc22e052bb2ef8da443
Moideen Nazaif VM <nazaifmoid@gmail.com>
2020-09-06 11:04:35 +0530

    Initial Commit

---
+++ CashRich Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CashRich Test
+//
+//  Created by Moideen Nazaif VM on 06/09/20.
+//  Copyright © 2020 Moideen Nazaif VM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
